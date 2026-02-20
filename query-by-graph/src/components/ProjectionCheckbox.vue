a473f4e09b3b3a7e3bace4928cebaee1be162402
Friedrich Answin Daniel Motz <hello@danielmotz.de>
2026-02-20 13:11:10 +0100

    fix: type

--- query-by-graph/src/components/ProjectionCheckbox.vue
+++ query-by-graph/src/components/ProjectionCheckbox.vue
@@ -4,7 +4,7 @@
       type="checkbox"
       :id="id"
       :checked="modelValue"
-      @change="$emit('update:modelValue', $event.target.checked)"
+      @change="$emit('update:modelValue', ($event.target as HTMLInputElement).checked)"
       class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 focus:ring-2"
     />
     <label :for="id" :class="['ml-2 text-sm font-medium cursor-pointer', labelClasses]">
